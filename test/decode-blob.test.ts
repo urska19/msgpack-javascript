3500558a40db2db3312d551b8c8f6ac04960e6eb
FUJI Goro <gfuji@cpan.org>
2020-03-31 12:52:20 +0900

    fix tests

--- test/decode-blob.test.ts
+++ test/decode-blob.test.ts
@@ -5,13 +5,16 @@ import { encode, decode, decodeAsync } from "@msgpack/msgpack";
 const MyBlob = typeof Blob !== "undefined" ? Blob : require("blob-polyfill").Blob;
 
 describe("Blob", () => {
-  it("decodes it with `decode()`", async () => {
+  it("decodes it with `decode()` and Blob#arrayBuffer()", async () => {
     const blob = new MyBlob([encode("Hello!")]);
     assert.deepStrictEqual(decode(await blob.arrayBuffer()), "Hello!");
   });
 
-  it("decodes it with `decodeAsync()`", async () => {
+  it("decodes it with `decodeAsync()` and Blob#stream()", async function () {
     const blob = new MyBlob([encode("Hello!")]);
+    if (blob.stream) {
+      this.skip();
+    }
     assert.deepStrictEqual(await decodeAsync(blob.stream()), "Hello!");
   });
 });
