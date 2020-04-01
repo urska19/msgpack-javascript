b0468d1c3c472b5e5cb8e7f98f1dd695cd69b1f8
FUJI Goro <gfuji@cpan.org>
2020-04-01 15:01:37 +0900

    fix tests (amended 3500558a40db2db3312d551b8c8f6ac04960e6eb)

--- test/decode-blob.test.ts
+++ test/decode-blob.test.ts
@@ -12,7 +12,7 @@ describe("Blob", () => {
 
   it("decodes it with `decodeAsync()` and Blob#stream()", async function () {
     const blob = new MyBlob([encode("Hello!")]);
-    if (blob.stream) {
+    if (!blob.stream) {
       this.skip();
     }
     assert.deepStrictEqual(await decodeAsync(blob.stream()), "Hello!");
